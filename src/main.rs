5673fc70002726eaf6fdc6488ee81178533c5f21
Forest828 <89926378+Forest828@users.noreply.github.com>
2024-08-30 00:38:20 +0200

    Add files via upload

--- src/main.rs
+++ src/main.rs
@@ -4,44 +4,46 @@ use std::io;
 
 fn main() {
     let mut vec: Vec<u8> = vec!(0; 3001);
-    let mut index: i32 = 1;
+    let mut index: usize = 1;
     let args: Vec<String> = env::args().collect();
     let path = &args[1];
     let contents = fs::read_to_string(path).expect("Unable to read file");
 
     for (j, i) in contents.chars().enumerate() {
         match i {
-            '+' => vec[index as usize] += 1,
-            '-' => vec[index as usize] -= 1,
+            '+' => vec[index] += 1,
+            '-' => vec[index] -= 1,
             '>' => index += 1,
             '<' => index -= 1,
-            '.' => print!("{}", vec[index as usize] as char),
+            '.' => print!("{}", vec[index] as char),
             ',' => {
                 let mut temp = String::new();
                 let _ = io::stdin().read_line(&mut temp);
                 let temp = (temp.as_bytes()[0] as char) as u8;
-                vec[index as usize] = temp;
+                vec[index] = temp;
             },
             '[' => {
-                //println!("loop");
-                let loop_variable: i32 = index;
-                let mut loop_index: i32 = (j + 1).try_into().unwrap();
-                let mut loop_section: String = String::new();                
-                while contents.as_bytes()[loop_index as usize] as char != ']'{
+                let loop_variable: usize = index;
+                let mut k: usize = (j + 1).try_into().unwrap();
+                let mut loop_section: String = String::new();
 
-                    loop_section.push(contents.as_bytes()[loop_index as usize] as char);
-                    loop_index += 1;
+                while contents.as_bytes()[k] as char != ']'{
+                    loop_section.push(contents.as_bytes()[k] as char);
+                    k += 1;
                 }
-                let mut loop_section_index: i32 = 0;
-                while vec[loop_variable as usize] != 0 {
-                    match loop_section.as_bytes()[loop_section_index as usize] as char {
-                        '+' => vec[index as usize] += 1,
-                        '-' => vec[index as usize] -= 1,
+
+                let mut loop_section_index: usize = 0;
+
+                while vec[loop_variable] != 0 {
+                    match loop_section.as_bytes()[loop_section_index] as char {
+                        '+' => vec[index] += 1,
+                        '-' => vec[index] -= 1,
                         '>' => index += 1,
                         '<' => index -= 1,
                         _ => {},
                     }
-                    if loop_section_index + 1 != loop_section.len() as i32 {
+                    
+                    if loop_section_index + 1 != loop_section.len() {
                         loop_section_index += 1;    
                     } else {
                         loop_section_index = 0;
